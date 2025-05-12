f83e24ef76b002853c38b3c14dd14fcec4ae3397
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-05-12 14:18:42 +0200

    improve the about part - still needs a group photo

--- scripts/build_css.sh
+++ scripts/build_css.sh
@@ -27,7 +27,7 @@ else
 fi
 if [ -f "$OUTFILE" ]; then
   echo "INFO: Running Tailwind CSS CLI..."
-  ./tailwindcss --input ./index.css --output ./static/css/index.css --verbose
+  ./tailwindcss --input ./index.css --output ./static/css/index.css --verbose $@
 else
   echo "ERROR: Failed to obtain 'tailwindcss' CLI."
   exit 1
